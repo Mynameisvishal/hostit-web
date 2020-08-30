5521f97759884eedc6c18dfe923e281f56f8d69e
Vishal P <vishalprakaash@github.com>
2020-08-30 17:11:19 +0530

    Updates

---
+++ precache-manifest.32310b8a197864d05a1feb77d8a9ea0f.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d11d42917bec13cb33e684b5b7cc70d",
+    "url": "/hostit-web/index.html"
+  },
+  {
+    "revision": "e0915cea1498e5b5dfbd",
+    "url": "/hostit-web/static/css/2.70cd73df.chunk.css"
+  },
+  {
+    "revision": "f9e5ccbd95b7754dbe8a",
+    "url": "/hostit-web/static/css/main.9760888e.chunk.css"
+  },
+  {
+    "revision": "e0915cea1498e5b5dfbd",
+    "url": "/hostit-web/static/js/2.0a9e423c.chunk.js"
+  },
+  {
+    "revision": "5bd09eb7f2cf4d145d532d862be18942",
+    "url": "/hostit-web/static/js/2.0a9e423c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e5ccbd95b7754dbe8a",
+    "url": "/hostit-web/static/js/main.85aab004.chunk.js"
+  },
+  {
+    "revision": "c66c778852a6c6415359",
+    "url": "/hostit-web/static/js/runtime-main.01723eeb.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/hostit-web/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/hostit-web/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/hostit-web/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/hostit-web/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/hostit-web/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/hostit-web/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/hostit-web/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/hostit-web/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/hostit-web/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/hostit-web/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/hostit-web/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "4557364a42d499a6eb8ed507ba30f09f",
+    "url": "/hostit-web/static/media/logo.4557364a.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/hostit-web/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/hostit-web/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/hostit-web/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/hostit-web/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/hostit-web/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
